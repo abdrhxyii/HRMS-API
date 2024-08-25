992f74a6af60ec50b3707e400404c097eb6de05f
abdrhxyii <abdrhxyii@git>
2024-08-25 22:36:12 +0530

    added repository, interfaces, services for employee

---
+++ Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+namespace HumanResource.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(){}
+        public NotFoundException(string message) : base(message) {}
+        public NotFoundException(string message, Exception innerexception) : base(message, innerexception) {}
+    }
+}
---
+++ Services/EmployeeService.cs
@@ -0,0 +1,72 @@
+using HumanResource.Data;
+using HumanResource.Exceptions;
+using HumanResource.Interfaces.IRepositories;
+using HumanResource.Interfaces.IServices;
+using HumanResource.Modals;
+using HumanResource.ViewModals;
+
+namespace HumanResource.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly ApplicationDbContext _context;
+        public EmployeeService(IEmployeeRepository employeeRepository, ApplicationDbContext context)
+        {
+            _employeeRepository = employeeRepository;
+            _context = context;
+        }
+
+        public async Task<EmployeeModal> AddEmplyeeAsync (EmployeeModal employeeModal)
+        {
+            if(employeeModal.CityId == null)
+            {
+                throw new ArgumentException("Please select a city");
+            }
+            if (employeeModal.StateId == null)
+            {
+                throw new ArgumentException("Please select a state");
+            }
+            return await _employeeRepository.AddEmployee(employeeModal);
+
+        }
+        public async Task<IEnumerable<EmployeeModal>> GetAllEmployeeAsync ()
+        {
+            var employees = await _employeeRepository.FindAllEmployee();
+            if(employees == null)
+            {
+                throw new NotFoundException("No Employee Found");
+            }
+            return employees;
+        }
+        public async Task<EmployeeModal> GetEmployeeByIdAsync(int id)
+        {
+            var employee = await _employeeRepository.FindEmployeeById(id);
+            if(employee == null)
+            {
+                throw new NotFoundException($"Employee with {id} cannot exist");
+            }
+            return employee;
+        }
+        public async Task<IEnumerable<EmployeeTableViewModal>> EmployeeViewTable()
+        {
+            var employees = await _employeeRepository.FindAllEmployee();
+            return employees.Select(x => new EmployeeTableViewModal{
+                EmployeeIdManual = x.EmployeeIdManual,
+                Image = x.Image,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Designation = x.Designation,
+                professionInfos = _context.ProfessionalEmployeeDetails
+                .Select(p => new EmployeeTableViewModal.ProfessionInfo{
+                    EmployeeType = p.EmployeeType,
+                    EmployeeStatus = p.EmployeeStatus
+                }).ToList()
+            }).ToList();
+        }
+        public async Task<bool> DeleteEmployeeByIdAsync(int id)
+        {
+            return await _employeeRepository.DeleteEmployeeById(id);
+        }    
+    }
+}
